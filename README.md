cd84c6daf4caa3f794a9009aec8707908eeb2584
MaryKaheti <kahetikihara@gmail.com>
2023-04-02 15:14:21 +0300

    Completed Assignment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # week-3-code-challenge
+
+## Goals
+Creation of a movie website called Flatdango which is a flatiron movie theater.
+Our website contains 15 movies which are listed from 1 - 15 respectively.
---
+++ index.js
@@ -0,0 +1,25 @@
+fetch('http://localhost:3000/films/1')
+  .then(response => response.json())
+  .then(data => {
+    const poster = document.createElement('img');
+    poster.src = data.poster;
+    document.body.appendChild(poster);
+    const title = document.createElement('h1');
+    title.textContent = data.title;
+    document.body.appendChild(title);
+
+    const runtime = document.createElement('p');
+    runtime.textContent = `Runtime: ${data.runtime} minutes`;
+    document.body.appendChild(runtime);
+
+    const showtime = document.createElement('p');
+    showtime.textContent = `Showtime: ${data.showtime}`;
+    document.body.appendChild(showtime);
+
+    const availableTickets = data.capacity - data.tickets_sold;
+    const tickets = document.createElement('p');
+    tickets.textContent = `Available Tickets: ${availableTickets}`;
+    document.body.appendChild(tickets);
+  })
+  .catch(error => console.error(error));
+  
