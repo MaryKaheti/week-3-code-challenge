f7d6199fc5f3a4d65813fcb3419ee075289a2a3a
MaryKaheti <kahetikihara@gmail.com>
2023-04-02 21:11:56 +0300

    Completed Assignment

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # week-3-code-challenge
 
 ## Goals
-Creation of a movie website called Flatdango which is a flatiron movie theater.
-Our website contains 15 movies which are listed from 1 - 15 respectively.
+- Creation of a movie website called Flatdango which is a flatiron movie theater.
+- Our website contains 15 movies which are listed from 1 - 15 respectively.
+- Use given information from APIs
+- Add events
+
+# 
--- index.js
+++ index.js
@@ -22,56 +22,12 @@ fetch('http://localhost:3000/films/1')
     document.body.appendChild(tickets);
   })
 
- const filmsList = document.querySelector("#films");
-
- fetch("http://localhost:3000/films")
-  .then((response) => response.json())
-  .then((films) => {
-    films.forEach((film) => {
-      const filmItem = document.createElement("li");
-      filmItem.textContent = film.title;
-      filmItem.classList.add("film", "item");
-      filmItem.addEventListener("click", () => {
-     const poster = document.createElement('img');
-     poster.src = data.poster;
-     document.body.appendChild(poster);
-     const title = document.createElement('h1');
-     title.textContent = data.title;
-     document.body.appendChild(title);
-
-     const runtime = document.createElement('p');
-     runtime.textContent = `Runtime: ${data.runtime} minutes`;
-     document.body.appendChild(runtime);
-
-     const showtime = document.createElement('p');
-     showtime.textContent = `Showtime: ${data.showtime}`;
-     document.body.appendChild(showtime);
-
-     const availableTickets = data.capacity - data.tickets_sold;
-     const tickets = document.createElement('p');
-     tickets.textContent = `Available Tickets: ${availableTickets}`;
-      document.body.appendChild(tickets);
-        console.log(film);
-      });
-      filmsList.appendChild(filmItem);
-    });
-  });
+ 
 
 
-const buyButtons = document.querySelectorAll('.buy-button');
 
 
-buyButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const movieId = button.dataset.movieId;
-    const ticketCount = document.querySelector(`#movie-${movieId}-ticket-count`);
-    let currentCount = parseInt(ticketCount.textContent);
-   if (currentCount > 0) {
-      currentCount--;
-      ticketCount.textContent = currentCount;
-    }
-  });
-});
+
 
 
   
